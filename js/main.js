f7773c5a72f39d481ab14c5def7bdaadacadbdb6
GRomanDev <grromf@gmail.com>
2020-07-22 14:17:35 +0800

    added: yandex map by JS API in hotel section

--- js/main.js
+++ js/main.js
@@ -11,3 +11,36 @@ var mySwiper = new Swiper(".swiper-container", {
     prevEl: ".slider-button__prev",
   },
 });
+
+// Дождёмся загрузки API и готовности DOM.
+ymaps.ready(init);
+
+function init() {
+  // Создание экземпляра карты и его привязка к контейнеру с
+  // заданным id ("map").
+  var myMap = new ymaps.Map(
+    "map",
+    {
+      // При инициализации карты обязательно нужно указать
+      // её центр и коэффициент масштабирования.
+      center: [7.890703, 98.294772],
+      zoom: 15,
+    },
+    {
+      searchControlProvider: "yandex#search",
+    }
+  );
+  // Создание геообъекта с типом точка (метка).
+  var myGeoObject = new ymaps.GeoObject({
+    geometry: {
+      type: "Point", // тип геометрии - точка
+      coordinates: [7.890703, 98.294772], // координаты точки
+    },
+    properties: {
+      iconCaption: "DoubleTree by Hilton Phuket Banthai Resort",
+    },
+  });
+
+  // Размещение геообъекта на карте.
+  myMap.geoObjects.add(myGeoObject);
+}
