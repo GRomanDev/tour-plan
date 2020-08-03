26c9d5563dc2dcf2ab8e5f80336c2b98afef32ae
GRomanDev <grromf@gmail.com>
2020-08-04 02:17:13 +0800

    create: added Form validation

--- js/main.js
+++ js/main.js
@@ -38,11 +38,11 @@ $(document).ready(function () {
   modalButton.on("click", openModal);
   closeModalButton.on("click", closeModal);
 
-  function closeModal(e) {
-    if (!e.keyCode | s | (e.keyCode === 27)) {
-    }
-  }
-  document.addEventListener("keydown", closeModal);
+  // function closeModal(e) {
+  //   if (!e.keyCode | s | (e.keyCode === 27)) {
+  //   }
+  // }
+  // document.addEventListener("keydown", closeModal);
 
   function openModal() {
     var targetModal = $(this).attr("data-href");
@@ -56,4 +56,23 @@ $(document).ready(function () {
     modalOverlay.removeClass("modal__overlay--visible");
     modalDialog.removeClass("modal__dialog--visible");
   }
+  // Обработка форм
+  $(".form").each(function () {
+    $(this).validate({
+      errorClass: "invalid",
+      messages: {
+        name: {
+          required: "Please specify your name",
+          minlength: "At least 2 characters required",
+        },
+        email: {
+          required: "We need your email address to contact you",
+          email: "Your email address must be in the format of name@domain.com",
+        },
+        phone: {
+          required: "Phone is required",
+        },
+      },
+    });
+  });
 });
